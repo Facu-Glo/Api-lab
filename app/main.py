e0c6eecae65ff7256f0350171b0c2125b62c0833
Facu <gabylope05@gmail.com>
2025-08-03 13:29:27 -0300

    refactor

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
-from typing import List
-from app.database.db import Base, engine, get_db
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from sqlalchemy.orm import Session
 
-from app.schemas.user import UserCreate, UserOut
-from app.models.user import User
+from app.database.db import Base, engine
+from app.routes import user
 
 # Creo las tablas
 Base.metadata.create_all(bind=engine)
@@ -18,16 +15,4 @@ def root():
     return RedirectResponse(url="/docs")
 
 
-# Crear usuario
-@app.post("/users/", response_model=UserOut)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    new_user = User(name=user.name, email=user.email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-@app.get("/users/", response_model=List[UserOut])
-def get_user(db: Session = Depends(get_db)):
-    return db.query(User).all()
+app.include_router(user.router)
---
+++ app/routes/user.py
@@ -0,0 +1,45 @@
+# from typing import List
+import math
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.orm import Session
+
+from app.database.db import get_db
+from app.schemas.user import UserCreate, UserOut
+from app.models.user import User
+
+router = APIRouter(prefix="/user", tags=["Users"])
+
+
+@router.post("/", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/paginated")
+def get_users_paginated(
+    db: Session = Depends(get_db),
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=100),
+):
+    total = db.query(User).count()
+    total_pages = math.ceil(total / limit)
+    skip = (page - 1) * limit
+    users = db.query(User).offset(skip).limit(limit).all()
+
+    return {
+        "total": total,
+        "total_pages": total_pages,
+        "page": page,
+        "limit": limit,
+        "results": users,
+    }
+
+
+@router.get("/{user_id}", response_model=UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).get(user_id)
+    return UserOut.model_validate(user)
