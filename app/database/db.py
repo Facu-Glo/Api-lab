01cfd0e08fd5b464426048e5a3d85028056d8717
Facu <gabylope05@gmail.com>
2025-08-03 01:09:57 -0300

    hidden credentials

--- app/database/db.py
+++ app/database/db.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 # 1. URL de conexión
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/biblioteca"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # 2. Crear el motor
 engine = create_engine(DATABASE_URL)
 # 3. Crear la fábrica de sesiones
